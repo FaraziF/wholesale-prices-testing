9d573522b9f37dd443d54d6bcefcb1ed7aeb2650
FaraziF <farazi333@gmail.com>
2025-08-11 21:31:40 +0600

    Congifuration file prepare for Cypress

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    },
+    baseUrl: "http://localhost:10003",
+  },
+});
